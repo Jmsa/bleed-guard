cc01460b66127e33910a419ccc74e4bfbd680b18
James Abercrombie <jmsabercrombie88@gmail.com>
2024-10-02 14:07:16 -0700

    chore: update jest reporter to use new "library" option

--- reporters/jest/jest.ts
+++ reporters/jest/jest.ts
@@ -8,6 +8,7 @@ export default class BleedReporter {
 
         // Store options
         this.options = {
+            library: "Jest",
             ...defaultOptions,
             ...options
         };
